3ff6fdd27065aedb04692b988a7a76fa8f01e001
Meng Seong Lee <melee@u82z01s05.42kl.edu.my>
2023-07-21 18:10:39 +0800

    done

--- mini_cd.c
+++ mini_cd.c
@@ -57,6 +57,29 @@ char	*get_cd(t_shell *shell)
 	return (NULL);
 }
 
+void	set_home(t_shell *shell)
+{
+	char	*home_path;
+	int		set_home;
+	int		i;
+
+	set_home = 0;
+	i = 0;
+	while (shell->local_envp[i])
+	{
+		if (!ft_strncmp(shell->local_envp[i], "HOME=", ft_strlen("HOME=")))
+		{
+			home_path = ft_strchr(shell->local_envp[i], '=');
+			home_path++;
+			chdir(home_path);
+			set_home = 1;
+		}
+		i++;
+	}
+	if (!set_home)
+		printf("bash: cd: HOME not set\n");
+}
+
 void	wait_cd(t_shell *shell)
 {
 	if (shell->pid_count == 1 && check_cd(shell)
@@ -64,7 +87,7 @@ void	wait_cd(t_shell *shell)
 		&& WEXITSTATUS(shell->exit_status) == EXIT_SUCCESS)
 	{
 		if (get_cd(shell) == NULL)
-			chdir("..");
+			set_home(shell);
 		else
 			chdir(get_cd(shell));
 	}
